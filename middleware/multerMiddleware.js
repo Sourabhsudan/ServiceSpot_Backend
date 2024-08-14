6fcab96516e44d171d3c8287b35fa6bba33546a7
Sourabh Sharma <117830711+Sourabhsudan@users.noreply.github.com>
2024-08-14 10:41:45 -0400

    Update multerMiddleware.js

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -19,11 +19,13 @@ const serviceAccount = {
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'servicespot-51527.appspot.com', // Replace with your Firebase Storage bucket URL
+  credential: admin.credential.cert({
+    project_id: serviceAccount.project_id,
+    client_email: serviceAccount.client_email,
+    private_key: serviceAccount.private_key.replace(/\\n/g, '\n'),
+  }),
+  storageBucket: 'servicespot-51527.appspot.com',
 });
-
-const bucket = admin.storage().bucket();
 
 // Configure Multer for file upload
 const storage = multer.memoryStorage();
